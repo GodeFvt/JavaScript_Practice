7eccc28a6811c3e64f85146dae6d58b6d99d9332
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2023-09-06 02:00:11 +0700

    a

--- Codewars/WeIrD_StRiNg_CaSe.js
+++ Codewars/WeIrD_StRiNg_CaSe.js
@@ -1,11 +1,17 @@
 function toWeirdCase(string) {
-  let word = string.split(" ").join(" ");
-  let str = word.match(/.{2}/g);
-  for (let i = 0; i < str.length; i++) {
-    str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+  let word = string.split("");
+  let count = 0;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+      count = 0;
+    } else if (count % 2 === 0) {
+      word[i] = word[i].toUpperCase();
+      count++;
+    } else {
+      count++;
+    }
   }
-  // return str.join('')
-  return word;
+  return word.join('');
 }
 console.log(toWeirdCase("This is a test"));
 // https://www.codewars.com/kata/52b757663a95b11b3d00062d/train/javascript
