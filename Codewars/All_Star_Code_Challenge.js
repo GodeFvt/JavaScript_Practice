b1291305a447f01b0a6efbff4da17870c711a3e5
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2023-10-03 23:59:41 +0700

    3/10/66

--- Codewars/All_Star_Code_Challenge.js
+++ Codewars/All_Star_Code_Challenge.js
@@ -1,14 +1,13 @@
-function rotate(str){
-    const result =[]
-    for(let i = 0 ; i < str.length ; i++){
-        const arr = str.split("");
-        const firstElement = arr.shift();
-        const newStr = arr.join("");
-        str = newStr + firstElement
-        result.push(str);   
-        
-    }
-    return result
+function rotate(str) {
+  const result = [];
+  for (let i = 0; i < str.length; i++) {
+    const arr = str.split("");
+    const firstElement = arr.shift();
+    const newStr = arr.join("");
+    str = newStr + firstElement;
+    result.push(str);
+  }
+  return result;
 }
-
+// https://www.codewars.com/kata/586560a639c5ab3a260000f3
 console.log(rotate("Hello"));
